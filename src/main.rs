8f8e57cdfd300dac1e57d481cc793e92eefe4ada
nazo6 <git@nazo6.dev>
2026-04-06 16:34:25 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use esp_idf_svc::hal::peripherals::Peripherals;
+
 mod lcd;
 mod platform;
 mod rgb565;
@@ -5,14 +7,12 @@ mod rgb565;
 slint::include_modules!();
 
 fn main() {
-    // It is necessary to call this function once. Otherwise, some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
-
-    // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    let peripherals = Peripherals::take().expect("Failed to take peripherals");
+
+    log::info!("App start");
 
     let mut display = lcd::Display::new().expect("Failed to initialize LCD");
     platform::run_ui(&mut display);
